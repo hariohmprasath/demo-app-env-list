dd39a6c8e8602508d3917c162511966ce8720aea
Hari Ohm Prasath <hariohmprasath@gmail.com>
2023-01-12 00:33:39 -0800

    Update DemoAppApplication.java

--- src/main/java/com/example/demoapp/DemoAppApplication.java
+++ src/main/java/com/example/demoapp/DemoAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoAppApplication.class, args)
+		SpringApplication.run(DemoAppApplication.class, args);
 	}
 
 }
